c0bd1234e4967dc50dbb0b181d2d31276a73d31f
Steven127413 <steven940103@gmail.com>
2025-06-11 15:57:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to NUK</title>
+</head>
+<body bgcolor="lightgreen">
+    <a href="form.php" target="_blank"><h1>Click Me</h1></a><br>
+    <img src="rick-astley-rick-roll.gif" width="900" border="1">
+</body>
+</html>
